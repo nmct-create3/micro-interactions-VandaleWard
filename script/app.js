25a0b7fbd5d237f4aa3b6547eb8b0b61bca4c856
Vandale Ward <55786685+VandaleWard@users.noreply.github.com>
2020-10-07 09:34:56 +0200

    added toggle password

--- script/app.js
+++ script/app.js
@@ -1,13 +1,34 @@
+let email = {}, password = {}, signInButton;
+
+function getDOMElements(){
+    
+}
+
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+    let checkbox = document.querySelector(`.c-toggle-password__checkbox`);
+    let passwordInput = document.querySelector(`.c-toggle-password__input`);
+    checkbox.addEventListener(`change`, function(){
+
+        if(checkbox.checked){
+            passwordInput.type = "text";
+            
+            console.log("checked")
+        }
+        else{
+            passwordInput.type = "password";
+            console.log("unchecked")
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
+    getDOMElements();
     handleFloatingLabel();
     handlePasswordSwitcher();
 });
